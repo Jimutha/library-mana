054b9e54364977e56dd83c25048bd4826f301128
Jimutha <jimuthasr11@gmail.com>
2025-08-19 12:47:29 +0530

    create file structure-backend

---
+++ backend/controllers/analyticsController.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function analyticsController() {
+  return <div></div>;
+}
---
+++ backend/controllers/loanController.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function loanController() {
+  return <div></div>;
+}

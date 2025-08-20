d0dbe4373ee461a2b94c647a46be7d781c4265f1
Jimutha <jimuthasr11@gmail.com>
2025-08-20 14:17:42 +0530

    completing middleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,5 +1,17 @@
-import React from "react";
+export const notFound = (req, res, next) => {
+  res.status(404);
+  next(new Error(`Not Found - ${req.originalUrl}`));
+};
 
-export default function errorMiddleware() {
-  return <div></div>;
-}
+export const errorHandler = (err, req, res, next) => {
+  // eslint-disable-line
+  const status =
+    res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(status).json({
+    success: false,
+    error: {
+      message: err.message || "Server Error",
+      stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
+    },
+  });
+};
